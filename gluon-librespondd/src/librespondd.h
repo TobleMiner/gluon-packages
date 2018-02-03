26909fe67a5629179b1daafd158e429d5e4dc62b
Tobias Schramm <tobleminer@gmail.com>
2018-02-04 00:33:17 +0100

    Do some things

---
+++ gluon-librespondd/src/librespondd.h
@@ -0,0 +1,16 @@
+#ifndef _LIBRESPONDD_H_
+#define _LIBRESPONDD_H_
+
+#include <stdint.h>
+
+enum {
+	RESPONDD_CB_OK,
+	RESPONDD_CB_CANCEL,
+	RESPONDD_CB_ERROR
+};
+
+typedef int (*)(char* json_data, size_t data_len, void* priv) respondd_cb;
+
+int request(const struct ip6_inaddr*, const char* query, unsigned long timeout_ms, respondd_cb callback, void* cb_priv);
+
+#endif
